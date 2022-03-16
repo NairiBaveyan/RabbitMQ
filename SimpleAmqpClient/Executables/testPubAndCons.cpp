342d8e5d6c906df1b18c7008d20b7c24d6792154
Nairi Baveyan <86774021+NairiBaveyan@users.noreply.github.com>
2022-03-16 13:48:26 +0400

    Update testPubAndCons.cpp

--- SimpleAmqpClient/Executables/testPubAndCons.cpp
+++ SimpleAmqpClient/Executables/testPubAndCons.cpp
@@ -9,14 +9,16 @@ using namespace AmqpClient;
 
 int main()
 {
-    Channel::ptr_t connection = Channel::Create("127.0.0.1",5672,"guest","guest");
+    //Channel::OpenOpts opts  = Channel::OpenOpts();
+    Channel::OpenOpts opts = Channel::OpenOpts::FromUri("amqp://[guest[:guest]@]");
+    Channel::ptr_t connection = Channel::Open(opts);
     const std::string body("message");
     BasicMessage::ptr_t out_message = BasicMessage::Create(body);
     connection->BasicPublish("N-exchange", "N-key",BasicMessage::Create("Message1"));
     std::string consumer_tag=connection->BasicConsume("N-queue","");
-Envelope::ptr_t env = connection->BasicConsumeMessage(cons_tag);
-        connection->BasicAck(env->GetDeliveryInfo());
-        BasicMessage::ptr_t bodyBasicMessage=env->Message();
-        std::string message=bodyBasicMessage->Body();
-        std::cout<<"message -> "<<message<<std::endl;
+    Envelope::ptr_t env = connection->BasicConsumeMessage(consumer_tag);
+    connection->BasicAck(env->GetDeliveryInfo());
+    BasicMessage::ptr_t bodyBasicMessage=env->Message();
+    std::string message=bodyBasicMessage->Body();
+    std::cout<<"message -> "<<message<<std::endl;
 }
